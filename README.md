9c75388d9d2d9f6335f2174c177f5eedee9fae3d
EC2 Default User <ec2-user@ip-172-16-27-36.ec2.internal>
2021-07-25 06:31:37 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
-# sagemaker-ml-cicd-pipeline-xgboost
+# Amazon SageMaker での CI デモ
+
+本サンプルコードは、機械学習モデル開発へのガバナンス強化を目的に、継続的インテグレーションのビルドプロジェクトの中でモデルの学習を行うものです。ビルド中に実行するワークフローは AWS Step Fuinctions で定義してあり、AWS Step Functions Data Science SDK を用いて Python で `pipeline.py` で定義してあります。ワークフロー中でのデータの前処理や学習の実行には Amazon SageMaker を活用しています。 AWS CodeBuild でのビルド仕様は `buildspec.yaml` に定義してあります。
+
+## 実行手順
+### Step1. GtiHub リポジトリをフォークする
+本サンプルコードは GitHub リポジトリと AWS CodeBuild を連携させてビルド中に機械学習モデルの学習を行います。このプロセスはリポジトリへのプッシュをトリガーにするため、 後ほど GitHub のアカウントと CodeBuild の接続が必要になります。その前準備として、このハンズオンの[リポジトリ](https://github.com/tkazusa/sagemaker-xgboost-cicd)をフォークします。
+
+
+### Step2. SageMaker notebook を作成する
+Amazon SageMaker ノートブックインスタンスを立ち上げます。
+
+- ロールを新規に作成し、任意の S3 バケットへのアクセス権限を付与する。
+- `Clone a public Git repository to this notebook instance only`  を選択し、このリポジトリをクローン
+- ノートブックインスタンスが ‘InService’ になるのを確認
+
+
+### Step3. データを Amazon S3 バケットへアップロードする。
+本ハンズオンでは、ニューヨークのタクシー運賃データを活用します。
+- `dataprep.ipynb` を実行し、末尾のセルを実行した際に表示されているバケット名を保存する。
+
+
+### Step4. IAM ロールを作成する
+データの前処理、学習といったステップやパイプラインの実行において AWS リソースが活用する IAM ロールを準備します。 IAM ロールの作成については、[こちら](https://docs.aws.amazon.com/ja_jp/IAM/latest/UserGuide/id_roles_create.html)をご確認ください。
+
+- AWS Glue の IAM ロールへ付与するポリシー: `AWSGlueServiceRole`, `S3FullAccess`
+- SageMaker の IAM ロールへ付与するポリシー: `AWSStepFunctionsFullAccess`, `AmazonSageMakerFullAccess`
+- AWS StepFunctions の IAM ロールへ付与するポリシー: `AWSGlueServiceRole`, `AmazonSageMakerFullAccess`, `AWSStepFunctionsFullAccess`
+    
+  
+それぞれ作成したロールの ARN は後ほど活用するので保存しておいて下さい。
+
+
+### Step.5 AWS CodeBuild のビルドプロジェクトを作成する
+
+本ハンズオンでは CodeBuild でのビルド中に機械学習モデルの学習を行います。
+AWS CodeBuildから、「ビルドプロジェクト」→「ビルドプロジェクトを作成する」へ進んで下さい。ビルドプロジェクトの作成画面で設定を行っていきます。
+
+まずは、プロジェクト名を指定します。
+
+![img1](img/img1.png)
+
+ソースプロバイダの設定を行います。ソースプロバイダを GitHub にし、OAuth もしくはアカウントトークンを使って GitHub と接続します。
+
+![img2](img/img2.png)
+
+クローンしてきた GitHub リポジトリを連携するよう指定します。
+
+![img3](img/img3.png)
+
+
+ウェブフックイベントをプルリクエストの作成と更新時に設定します。
+
+![img4](img/img4.png)
+
+
+ビルド環境の設定を下記のように行います。
+
+![img5](img/img5.png)
+
+
+ビルドプロジェクト作成時に作成した `codebuild-sagemaker-ci-pipeline-xgboost-service-role` へ下記のポリシーを追加します。
+
+- `StepFunctionsFullAccess`
+- `AWSCodebuildDeveloperAccess`
+- `AmazonS3FullAccess`
+- `AWSGlueServiceRole`
+    
+下記ポリシーを作成して追加します。
+```JSON
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Action": [
+				"iam:PassRole"
+			],
+			"Effect": "Allow",
+			"Resource": "*"
+		}
+	]
+}
+```
+
+
+### Step6. ビルドプロジェクトの実行
+SageMaker Notebook 上からスクリプトへの変更を実行し、プルリクエストを更新することで、ビルドプロジェクトを実行します。
+
+- SageMaker ノートブックインスタスにて JupyterLab を開きます。
+- ターミナルを起動して、新しいブランチ `model-dev` を作成し、チェックアウトします。
+
+```Bash
+$ cd SageMaker/
+$ git checkout -b model-dev
+```
+
+- `pipeline.py` 中の下記項目を設定します。
+
+```Python
+BUCKET='<データを準備した際に保存したバケット>'
+FLOW_NAME='flow_{}'.format(id) 
+TRAINING_JOB_NAME='sf-train-{}'.format(id) # To avoid duplication of job name
+GLUE_ROLE = '<Glue に付与するロール>'
+SAGEMAKER_ROLE = '<SageMaker に付与するロール>'
+WORKFLOW_ROLE='<Step Functions に付与するロール>'
+```
+
+git 上に変更を反映します。
+```Bash
+$ git add pipeline.py
+$ git commit -m “mod pipeline.py”
+$ git push origin HEAD
+```
+
+GitHub リポジトリ上で Pull Request を作成します。
+
+
---
+++ pipeline.py
@@ -0,0 +1,142 @@
+import os
+import uuid
+import logging
+import argparse
+
+import stepfunctions
+import boto3
+import sagemaker
+from sagemaker import get_execution_role
+from sagemaker.estimator import Estimator
+from sagemaker.session import s3_input
+from sagemaker.amazon.amazon_estimator import get_image_uri
+
+from stepfunctions import steps
+from stepfunctions.inputs import ExecutionInput
+from stepfunctions.workflow import Workflow
+
+stepfunctions.set_stream_logger(level=logging.INFO)
+id = uuid.uuid4().hex
+
+REGION='us-east-1'
+BUCKET='<データを準備した際に指定したバケット>'
+FLOW_NAME='flow_{}'.format(id) 
+TRAINING_JOB_NAME='sf-train-{}'.format(id) # To avoid duplication of job name
+GLUE_ROLE = '<your-glue-role>'
+SAGEMAKER_ROLE = '<your-sagemaker-role>'
+WORKFLOW_ROLE='<your-stepfunctions-role>'
+
+
+if __name__ == '__main__':
+    # buildspec.yaml の env.variables へ直接書き込んでだ場合、下記のように環境変数から設定を読み込んでくる
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('--data_path', type=str, default=os.environ['DATA_PATH'])
+    #args = parser.parse_args()
+
+    s3 = boto3.resource('s3')
+    session = sagemaker.Session()
+
+    # SFn の実行に必要な情報を渡す際のスキーマを定義します
+    execution_input = ExecutionInput(schema={
+        # AWS Glue
+        'bucket_name': str,
+
+        # SageMaker
+        'TrainJobName': str,
+        }
+    )
+
+    # それぞれのステップを定義していきます
+    ## AWS Glue の job を Submit するステップ
+    ## バケットへの preprocess.py のアップロード
+    s3.Object(BUCKET, 'preprocess.py').upload_file('preprocess.py')
+    print('s3://{} へ Glue のスクリプトがアップロードされました。'.format(BUCKET))
+
+    # Glue のジョブを作成します。
+    glue = boto3.client('glue')
+    job = glue.create_job(
+                        Name='preprocess',
+                        Role=GLUE_ROLE,
+                        Command={'Name': 'pythonshell',
+                               'ScriptLocation': 's3://{}/preprocess.py'.format(BUCKET),
+                               'PythonVersion': '3'}
+                        )
+    # StepFunctions で実行する Step として Glue Job を設定します
+    etl_step = steps.GlueStartJobRunStep(
+        state_id='GlueDataProcessingStep',
+        parameters={
+            'JobName': job['Name'],
+            'Arguments': {
+                '--bucket_name': execution_input['bucket_name'],
+                }
+            }
+        )
+
+
+    ## Amazon SageMaker の学習ジョブを作成
+    ## データのパスを指定
+    input_train = 's3://{}/train.csv'.format(BUCKET)
+    input_validation = 's3://{}/validation.csv'.format(BUCKET)
+
+    s3_input_train = s3_input(s3_data=input_train, content_type='text/csv')
+    s3_input_validation = s3_input(s3_data=input_validation, content_type='text/csv')
+
+    container = get_image_uri(boto3.Session().region_name, 'xgboost')
+    hyperparameters = {
+            "max_depth": "5",
+            "eta": "0.2",
+            "gamma": "4",
+            "min_child_weight": "6",
+            "subsample": "0.8",
+            "silent": "0",
+            "objective": "reg:linear",
+            "num_round": "100"
+            }
+    
+    xgb = sagemaker.estimator.Estimator(
+        container,
+        hyperparameters=hyperparameters,
+        role=SAGEMAKER_ROLE,
+        train_instance_count = 1,
+        train_instance_type = 'ml.m4.4xlarge',
+        train_volume_size = 5,
+        sagemaker_session = session
+    )
+
+    # 学習ジョブを Step として設定
+    training_step = steps.TrainingStep(
+        'Train Step', 
+        estimator=xgb,
+        data={
+            'train': s3_input_train, 
+            'validation': s3_input_validation
+        },
+        job_name=execution_input['TrainJobName'],  
+        wait_for_completion=True 
+    )
+
+    # このあと model_step/batch_tramsformStep: https://towardsdatascience.com/automating-machine-learning-workflows-with-aws-glue-sagemaker-and-aws-step-functions-data-science-b4ed59e4d7f9
+    
+    # 各 Step を連結
+    chain_list = [etl_step, training_step]
+    workflow_definition = steps.Chain(chain_list)
+
+    # Workflow の作成
+    workflow = Workflow(
+        name=FLOW_NAME,
+        definition=workflow_definition,
+        role=WORKFLOW_ROLE,
+        execution_input=execution_input,
+    )
+    workflow.create()
+
+    # Workflow の実行
+    ## SFn のワークフローの定義を記載します
+    inputs={
+         # AWS Glue
+        'bucket_name': BUCKET,
+        # SageMaker Training
+        'TrainJobName': TRAINING_JOB_NAME
+        }
+
+    execution = workflow.execute(inputs=inputs)
